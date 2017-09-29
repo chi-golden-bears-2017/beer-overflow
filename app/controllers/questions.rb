56fab79236129e6d5b989ef551a46fd12f6185d1
Gilad Shanan & Nicole Cody <pair+giladshanan.ncody@devbootcamp.com>
2017-09-29 11:30:52 -0500

    add vote buttons, create votes at post route

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -7,3 +7,16 @@ get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :'questions/show'
 end
+
+post '/questions/:id/votes' do
+  p params[:value]
+  @question = Question.find(params[:id])
+  @vote = Vote.new(
+    value: params[:value],
+    user_id: current_user.id,
+    votable: @question)
+  p @vote.save
+  p @vote
+
+  # erb :'questions/show'
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $("#login-button").on('click', showForm);
   $("#login-form").on('submit', loginHandler);
+  $("#vote-buttons button").on('click', voteHandler);
+
 });
 
 var showForm = function(event) {
@@ -20,12 +22,36 @@ var loginHandler = function(event) {
   })
 
   ajaxPromise.done(function(response) {
-    console.log("success");
-    console.log(response)
     $this.hide();
     $(".logged-out").hide();
     $(".logged-in li").first().text("Hello, " + response + "!")
     $(".logged-in").show();
   })
+};
 
+var voteHandler = function(event) {
+  // event.preventDefault();
+  var button = $(this)
+  console.log(button)
+  var questionId = $('h2').attr('id')
+  var url = '/questions/' + questionId + '/votes'
+  var data;
+  if (button.attr('id') === "up-vote"){
+    data = {value: 1}
+  } else {
+    data = {value: -1}
+  }
+
+  var ajaxPromise = $.ajax({
+    url: url,
+    method: 'POST',
+    data: data
+  })
+
+  ajaxPromise.done(function(response) {
+    // $this.hide();
+    // $(".logged-out").hide();
+    // $(".logged-in li").first().text("Hello, " + response + "!")
+    // $(".logged-in").show();
+  })
 };
